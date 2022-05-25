932440b27cf2eda0aa1f2bbbec76f5749f47b2ce
RiRyCasals <rirycasals@gmail.com>
2022-05-25 18:42:54 +0900

    占いアプリ

---
+++ src/subject20220521/FortuneTelling/compile.sh
@@ -0,0 +1,3 @@
+javac -cp $CATALINA_HOME/lib/servlet-api.jar\
+        -d $CATALINA_HOME/webapps/FortuneTelling/WEB-INF/classes\
+        $CATALINA_HOME/webapps/FortuneTelling/WEB-INF/classes/SampleServlet.java
